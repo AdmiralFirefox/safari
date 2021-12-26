544e803b1f2fd521602e78a745916964f09314e2
AdmiralFirefox <jjlcuadra@gmail.com>
2021-12-26 21:24:13 +0800

    Converted CSS to SASS files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import homeStyles from "../styles/Home.module.css";
+import homeStyles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
