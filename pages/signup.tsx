53d03e2963c5ef5c41ce42fd82ce04106aea4131
AdmiralFirefox <jjlcuadra@gmail.com>
2022-02-19 19:29:52 +0800

    Added Styling on Sign Up Page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,12 +1,20 @@
+import { useState, useContext, useRef, useEffect } from "react";
 import type { NextPage } from "next";
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
-import { useContext, useRef, useEffect } from "react";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import CircularProgress from "@mui/material/CircularProgress";
 import { AuthContext } from "../context/AuthContext";
 import { auth } from "../firebase/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import AccountButton from "../components/Button/AccountButton";
+import styles from "../styles/pages/SignUp.module.scss";
 
 const SignUp: NextPage = () => {
+  const [loading, setLoading] = useState(false);
+
   const router = useRouter();
   const user = useContext(AuthContext);
 
@@ -15,16 +23,18 @@ const SignUp: NextPage = () => {
 
   //Create Account
   const createAccount = async () => {
+    setLoading(true);
     try {
       await createUserWithEmailAndPassword(
         auth,
         emailRef.current!.value,
         passwordRef.current!.value
       );
-      alert("Sign Up Successful!");
+      setLoading(false);
     } catch (error) {
       console.error(error);
       alert(error);
+      setLoading(false);
     }
   };
 
@@ -36,23 +46,128 @@ const SignUp: NextPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
 
+  const loadingInfo = (): JSX.Element => {
+    if (loading) {
+      return (
+        <CircularProgress size={45} sx={{ color: "#000", marginTop: "1em" }} />
+      );
+    } else {
+      return (
+        <div className={styles["signup-button-wrapper"]}>
+          <AccountButton onButtonClick={createAccount}>
+            Create Account
+          </AccountButton>
+        </div>
+      );
+    }
+  };
+
   return (
     <div>
       {!user ? (
         <>
-          <h1>Sign Up</h1>
-          <h2>Email</h2>
-          <input ref={emailRef} type="email" placeholder="email" />
-          <h2>Password</h2>
-          <input ref={passwordRef} type="password" placeholder="password" />
-          <button onClick={createAccount} type="button">
-            Sign Up
-          </button>
+          <div className={styles["signup-web-logo"]}>
+            <Image
+              src="/assets/SafariLogoDark.png"
+              alt="Web Logo"
+              width={200}
+              height={70}
+              objectFit="cover"
+            />
+          </div>
 
-          <h1>Already have an Account?</h1>
-          <Link href="/login">
-            <a>Click Here to Login</a>
-          </Link>
+          <div className={styles["signup-wrapper"]}>
+            <div className={styles["signup-content"]}>
+              <h1 className={styles["signup-title"]}>Sign Up</h1>
+
+              <p className={styles["signup-form-label"]}>Email</p>
+              <Paper
+                sx={{
+                  p: "0.5em",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                  background: "#EAEDED",
+                  boxShadow: "none",
+                }}
+              >
+                <InputBase
+                  inputRef={emailRef}
+                  type="email"
+                  placeholder="Enter Email"
+                  inputProps={{ "aria-label": "email" }}
+                  sx={{ ml: 1, flex: 1, color: "#000", fontWeight: "700" }}
+                />
+              </Paper>
+
+              <p className={styles["signup-form-label"]}>Password</p>
+              <Paper
+                sx={{
+                  p: "0.5em",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                  background: "#EAEDED",
+                  boxShadow: "none",
+                }}
+              >
+                <InputBase
+                  inputRef={passwordRef}
+                  type="password"
+                  placeholder="Enter Password"
+                  inputProps={{ "aria-label": "password" }}
+                  sx={{ ml: 1, flex: 1, color: "#000", fontWeight: "700" }}
+                />
+              </Paper>
+
+              <p className={styles["signup-form-label"]}>Confirm Password</p>
+              <Paper
+                sx={{
+                  p: "0.5em",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                  background: "#EAEDED",
+                  boxShadow: "none",
+                }}
+              >
+                <InputBase
+                  inputRef={passwordRef}
+                  type="password"
+                  placeholder="Enter Password"
+                  inputProps={{ "aria-label": "password" }}
+                  sx={{ ml: 1, flex: 1, color: "#000", fontWeight: "700" }}
+                />
+              </Paper>
+
+              <p className={styles["signup-conditions"]}>
+                By Logging in, you agree to Safari&apos;s{" "}
+                <Link href="/login">
+                  <a className={styles["signup-conditions-highlights"]}>
+                    Conditions of Use
+                  </a>
+                </Link>{" "}
+                and{" "}
+                <Link href="/login">
+                  <a className={styles["signup-conditions-highlights"]}>
+                    Privacy Notice
+                  </a>
+                </Link>
+              </p>
+
+              {loadingInfo()}
+
+              <div className={styles["login-redirect"]}>
+                <p>Already have an Account?</p>
+                <Link href="/login">
+                  <a>Log In</a>
+                </Link>
+              </div>
+            </div>
+          </div>
         </>
       ) : null}
     </div>
