9ead99be4cfa515f626149d378b32e280989f7f1
AdmiralFirefox <jjlcuadradev@gmail.com>
2022-02-24 13:52:39 +0800

    Declared Types for Publishable Keys

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,8 @@
 @declare module "redux-persist/lib/storage"
+
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
+        STRIPE_SECRET_KEY: string;
+    }
+}
