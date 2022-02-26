cbdb6ad0795abfdded604353cdfd46aaa49e61a0
AdmiralFirefox <jjlcuadradev@gmail.com>
2022-02-26 20:26:32 +0800

    Added type declaration for stripe webhook secret key

--- env.d.ts
+++ env.d.ts
@@ -4,5 +4,6 @@ declare namespace NodeJS {
     export interface ProcessEnv {
         NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
         STRIPE_SECRET_KEY: string;
+        STRIPE_WEBHOOK_SECRET: string;
     }
 }
