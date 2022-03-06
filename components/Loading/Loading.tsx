4e7908ea7178c5230852e28ef131122cfd48fd86
AdmiralFirefox <jjlcuadradev@gmail.com>
2022-03-06 16:29:15 +0800

    Added Loading component

---
+++ components/Loading/Loading.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import Image from "next/image";
+import styles from "../../styles/loading/Loading.module.scss";
+
+interface LoadingProps {
+  title: string;
+}
+
+const Loading: FC<LoadingProps> = ({ title }) => {
+  return (
+    <div className={styles["loading-wrapper"]}>
+      <h1>{title}</h1>
+      <Image
+        src="/assets/DeliveryTruck.gif"
+        width={375}
+        height={300}
+        objectFit="contain"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Loading;
