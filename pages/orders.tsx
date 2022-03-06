5a71e16d89deedb3f9711b98112252154a13dc29
AdmiralFirefox <jjlcuadradev@gmail.com>
2022-03-06 21:29:53 +0800

    Added Styles on Orders Page

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -1,14 +1,27 @@
 import { useState, useEffect, useContext } from "react";
+import Image from "next/image";
 import type { NextPage } from "next";
 import { db } from "../firebase/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
 import { AuthContext } from "../context/AuthContext";
+import Rating from "@mui/material/Rating";
 import Placeholder from "../components/Placeholder/Placeholder";
 import EmptyPlaceholder from "../components/EmptyPlaceholder/EmptyPlaceholder";
+import dayjs from "dayjs";
+import Loading from "../components/Loading/Loading";
 import styles from "../styles/pages/Orders.module.scss";
 
 interface OrderProps {
   id: string;
+  createdAt?: {
+    seconds: number;
+  };
+  image?: string;
+  title?: string;
+  description?: string;
+  rating?: number;
+  quantity?: number;
+  price?: number;
 }
 
 const Orders: NextPage = () => {
@@ -19,16 +32,16 @@ const Orders: NextPage = () => {
   //Getting Orders from the collection
   useEffect(() => {
     setLoadingOrders(true);
-    const getOrders = async () => {
-      const orderData = await getDocs(collection(db, "orders"));
-      setOrders(orderData.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    const ordersRef = collection(db, "orders");
+    const q = query(ordersRef, orderBy("createdAt", "desc"));
+
+    const unsub = onSnapshot(q, (snapshot) => {
+      setOrders(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
       setLoadingOrders(false);
-    };
+    });
 
-    getOrders();
+    return () => unsub();
   }, []);
-
-  console.log(orders);
 
   //If the user is not logged in
   if (!user) {
@@ -57,12 +70,64 @@ const Orders: NextPage = () => {
   }
 
   if (loadingOrders) {
-    return <h1>Loading...</h1>;
+    return <Loading title="Loading your Orders..." />;
   }
 
   return (
     <>
-      <h1>Orders</h1>
+      <div className={styles["orders-title"]}>
+        <h1>Your Orders:</h1>
+      </div>
+
+      <div className={styles["orders-wrapper"]}>
+        <div className={styles["orders-container"]}>
+          {orders.map((order) => {
+            return (
+              <div key={order.id} className={styles["order-item"]}>
+                <p className={styles["order-date"]}>
+                  {dayjs(order.createdAt!.seconds * 1000).format(
+                    "MMMM D YYYY, h:mm:ss a"
+                  )}
+                </p>
+
+                <div className={styles["order-info"]}>
+                  <div>
+                    <Image
+                      src={order.image!}
+                      alt=""
+                      width={150}
+                      height={150}
+                      objectFit="contain"
+                    />
+                  </div>
+                  <div>
+                    <p className={styles["order-title"]}>{order.title}</p>
+                    <p className={styles["order-description"]}>
+                      {order.description}
+                    </p>
+                    <Rating
+                      name="Ordered Product Ratings"
+                      value={order.rating}
+                      precision={0.5}
+                      sx={{ marginTop: "1em" }}
+                      size="small"
+                      readOnly
+                    />
+                  </div>
+                </div>
+
+                <div className={styles["amount-info"]}>
+                  <p>Quantity: {order.quantity}</p>
+                  <p>Price: ${order.price!.toFixed(2)}</p>
+                  <p>
+                    Subtotal: ${(order.price! * order.quantity!).toFixed(2)}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
